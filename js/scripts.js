1c69c6699dd576af704488dd75575c037eada06b
garrettvercoe <garrettvercoe@gmail.com>
2020-03-30 15:15:39 -0400

    formatting fix, updated forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-var submissions = ["Testing 1 2 3", "A deeper sense of peace and ability to rest and slow down", "A bigger focus on healthcare organizations and sufficient supplies to prevent shortages in the future"]
-var dict = [{"message": "Testing 1 2 3", "date":"2020-01-01", "time":"2:46 PM"},{"message": "Testing 1 2 3", "date":"2020-01-01", "time":"2:46 PM"}]
-var divs = ''
+var submissions = [
+  "Testing 1 2 3",
+  "A deeper sense of peace and ability to rest and slow down",
+  "A bigger focus on healthcare organizations and sufficient supplies to prevent shortages in the future"
+];
+var dict = [
+  { message: "Testing 1 2 3", date: "2020-01-01", time: "2:46 PM" },
+  { message: "Testing 1 2 3", date: "2020-01-01", time: "2:46 PM" }
+];
+var divs = "";
 
 // function readTextFile(file)
 // {
@@ -23,24 +30,29 @@ var divs = ''
 var jsonResult;
 $.ajax({
   dataType: "json",
-  url: "https://gist.githubusercontent.com/garrettvercoe/4b217fb2a20e288414223db88f01a099/raw/a3084b6c5b4ea4ccabfbaa2071475df5e60eff52/gistfile1.txt",
+  url:
+    "https://gist.githubusercontent.com/garrettvercoe/a4456ea32a5a291dd46e5257715a4cb2/raw/f39fe92f92b1426fae9d5a8f9b52a2ee962655c5/cleaned_contact.json",
   success: function(e) {
     //    jsonResult = data;
     //    console.log(data)
-    console.log(e)
+    console.log(e);
     // jsonResult = JSON.parse(e)
     e.forEach(function(jsonItem) {
-        divs += '<div class="submission"> <div class="date">' + jsonItem.date + '</div>' + jsonItem.message + '</div>';
-      }); 
+      divs +=
+        '<div class="submission"> <div class="date">' +
+        jsonItem.date +
+        "</div>" +
+        jsonItem.message +
+        "</div>";
+    });
     document.getElementById("submissionContainer").innerHTML = divs;
-    }
+  }
 });
-
 
 // dict.forEach(function(dict) {
 //     divs += '<div class="submission"> <div class="date">' + dict.date + '</div>' + dict.message + '</div>';
-//   }); 
+//   });
 // jsonResult.forEach(function(jsonItem) {
 //     divs += '<div class="submission"> <div class="date">' + jsonItem.date + '</div>' + jsonItem.message + '</div>';
-//   }); 
+//   });
 // document.getElementById("submissionContainer").innerHTML = divs;
